cf05e2bc06b351c2c6620ab7a946a7ba86854b8d
micke-ckemi-ikor <nikola.micic@ikor.de>
2020-04-11 17:00:52 +0200

    init commit

---
+++ src/main/kotlin/io/mickeckemi21/kotlinforjavadeveloperscoursera/KotlinForJavaDevelopersCourseraApplication.kt
@@ -0,0 +1,11 @@
+package io.mickeckemi21.kotlinforjavadeveloperscoursera
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinForJavaDevelopersCourseraApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinForJavaDevelopersCourseraApplication>(*args)
+}
